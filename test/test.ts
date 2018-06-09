9cb1db6bfd7d04541c197746e9c6481d2cea41cc
Sergey Shandar <sergey.shandar@gmail.com>
2018-06-09 00:12:53 -0700

    asserts

--- test/test.ts
+++ test/test.ts
@@ -1,27 +1,25 @@
 import * as _ from "../index"
 import "mocha";
-import * as chai from "chai"
-
-chai.should()
+import { assert } from "chai"
 
 describe("map", () => {
     it("array", () => {
         const result = Array.from(_.map([1, 2, 3], x => x * x))
-        result.should.deep.equal([1, 4, 9])
+        assert.deepEqual([1, 4, 9], result)
     })
 })
 
 describe("flatten", () => {
     it("array", () => {
         const result = Array.from(_.flatten([[1, 2], [2, 3], [3, 4]]))
-        result.should.deep.equal([1, 2, 2, 3, 3, 4])
+        assert.deepEqual([1, 2, 2, 3, 3, 4], result)
     })
 })
 
 describe("flatMap", () => {
     it("array", () => {
         const result = Array.from(_.flatMap([1, 2, 3], x => [x, x * 2]))
-        result.should.deep.equal([1, 2, 2, 4, 3, 6])
+        assert.deepEqual([1, 2, 2, 4, 3, 6], result)
     })
 })
 
@@ -29,91 +27,91 @@ describe("groupBy", () => {
     it("array", () => {
         const m = _.map([1, 2, 3, 1, 3, 2, 3, 3], x => _.nameValue(x.toString(), 1))
         const result = _.groupBy(m, (a, b) => a + b)
-        result.should.deep.equal({ 1: 2, 2: 2, 3: 4 })
+        assert.deepEqual({ 1: 2, 2: 2, 3: 4 }, result)
     })
 })
 
 describe("values", () => {
     it("array", () => {
         const result = Array.from(_.values({ 1: 2, 2: 2, 3: 3 }))
-        result.should.deep.equal([2, 2, 3])
+        assert.deepEqual([2, 2, 3], result)
     })
 })
 
 describe("entries", () => {
     it("array", () => {
         const result = Array.from(_.entries({ 1: 2, 2: 2, 3: 3 }))
-        result.should.deep.equal([["1", 2], ["2", 2], ["3", 3]])
+        assert.deepEqual([["1", 2], ["2", 2], ["3", 3]], result)
     })
 })
 
 describe("repeat", () => {
     it("array", () => {
         const result = Array.from(_.repeat("Hello!", 5))
-        result.should.deep.equal(["Hello!", "Hello!", "Hello!", "Hello!", "Hello!"])
+        assert.deepEqual(["Hello!", "Hello!", "Hello!", "Hello!", "Hello!"], result)
     })
 })
 
 describe("zip", () => {
     it("array", () => {
         const result = Array.from(_.zip([1, "b"], ["a", 2]))
-        result.should.deep.equal([[1, "a"], ["b", 2]])
+        assert.deepEqual([[1, "a"], ["b", 2]], result)
     })
 })
 
 describe("generate", () => {
     it("infinite", () => {
         const result = Array.from(_.zip(_.repeat(2), [1, 3]))
-        result.should.deep.equal([[2, 1], [2, 3]])
+        assert.deepEqual([[2, 1], [2, 3]], result)
     })
 })
 
 describe("reduce", () => {
     it("no items", () => {
         const result = _.reduce([], a => a)
-        chai.assert.isUndefined(result)
+        assert.isUndefined(result)
     })
     it("1", () => {
         const result = _.reduce([1], (a, b) => a + b)
         if (result === undefined) {
             throw new Error("undefined")
         }
-        result.should.equal(1)
+        assert.equal(1, result)
     })
 })
 
 describe("sum", () => {
     it("array", () => {
         const result = _.sum([1, 2, 3])
-        result.should.equal(6)
+        assert.equal(6, result)
     })
 })
 
 describe("min", () => {
     it("3", () => {
         const result = _.min([1, 2, 3])
-        result.should.equal(1)
+        assert.equal(1, result)
     })
     it("0", () => {
         const result = _.min([])
-        result.should.equal(Infinity)
+        assert.equal(Infinity, result)
     })
 })
 
 describe("max", () => {
     it("3", () => {
         const result = _.max([1, 2, 3])
-        result.should.equal(3)
+        assert.equal(3, result)
     })
     it("0", () => {
         const result = _.max([])
-        result.should.equal(-Infinity)
+        assert.equal(-Infinity, result)
     })
 })
 
 describe("toObject", () => {
     it("toObject", () => {
         const result = _.toObject([["a", 2], ["b", 4]])
-        result.should.deep.equal({ a: 2, b: 4 })
+        assert.deepEqual({ a: 2, b: 4 }, result)
     })
 })
