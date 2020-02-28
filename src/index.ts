3a5542bda56e71115d4d61142da7284fe8973f7b
Phoenix He <tih@microsoft.com>
2020-02-28 11:57:11 +0800

    Fix iterator type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@
  * See this PR https://github.com/microsoft/TypeScript/pull/30790
  */
 
+import { IterableInternal } from "./iterableInternalType";
+
+export type Iterable<T> = IterableInternal<T>;
+
 export type IterableEx<T> = Iterable<T> & {
     /**
      * The function returns an iterator of a this container own enumerable number-keyed value [key, value] pairs.
